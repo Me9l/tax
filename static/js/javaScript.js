594908381b95fd356c6dc82acdb039d7a1614eb1
KU <vebvdev@gmail.com>
2023-06-29 00:45:27 +0900

    create about.html and modify css, add imgs

---
+++ static/js/javaScript.js
@@ -0,0 +1,23 @@
+let btnToTop = document.getElementById("btn-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    btnToTop.style.display = "block";
+  } else {
+    btnToTop.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+btnToTop.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
