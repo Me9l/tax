3f163440021a6c62ce2b691bfd5cd018f043ed92
KU <vebvdev@gmail.com>
2023-08-10 13:17:43 +0900

    inquiry javascript function ( agreeAll )

--- static/js/javaScript.js
+++ static/js/javaScript.js
@@ -25,3 +25,68 @@ function backToTop() {
 }
 
 // E : TOP BUTTON
+
+// S : Agreements
+
+const form = document.getElementById('formWrap');
+const checkAll = document.getElementById('agreeAll');
+const checkBox = document.querySelectorAll('.form-check-input');
+const submitButton = document.querySelector('.submit');
+
+const agreements = {
+  turmsOfUse : false,
+  privacy : false,
+  marketing : false,
+};
+
+form.addEventListener('submit',(e)=>e.preventDefault());
+
+checkBox.forEach((item)=>item.addEventListener('input',toggleCheckBox));
+
+function toggleCheckBox(e) {
+  const { checked, id } = e.target;
+  agreements[id] = checked;
+  this.parentNode.classList.toggle('active');
+  checkAllStatus();
+  toggleSubmitButton();
+}
+
+function checkAllStatus() {
+  const { turmsOfUse, privacy, marketing }  = agreements;
+  if (turmsOfUse && privacy && marketing) {
+    checkAll.checked = true;
+  } else {
+    checkAll.checked = false;
+  }
+}
+
+function toggleSubmitButton() {
+  const { turmsOfUse, privacy } = agreements;
+
+  if(turmsOfUse && privacy) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+}
+
+checkAll.addEventListener('click', (e)=> {
+  const { checked } = e.target;
+  if (checked) {
+    checkBox.forEach((item) => {
+      item.checked = true;
+      agreements[item.id] = true;
+      item.parentNode.classList.add('active');
+    });
+  } else {
+    checkBox.forEach((item)=>{
+      item.checked = false;
+      agreements[item.id] = false;
+      item.parentNode.classList.remove('active');
+    });
+  }
+  toggleSubmitButton();
+});
+
+
+// E : Agreements
